ced4f3ebd1f4e2ec8fae39819c71aa66ef422c12
SHY <shy9729@gmail.com>
2026-01-16 14:28:12 +0900

    style: 안쓰는 import 삭제
    안쓰는 import 삭제

--- src/main/java/com/example/lineofduty/common/exception/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/example/lineofduty/common/exception/JwtAuthenticationEntryPoint.java
@@ -1,12 +1,9 @@
 package com.example.lineofduty.common.exception;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
