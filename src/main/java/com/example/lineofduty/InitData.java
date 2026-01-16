511c3dde18ee2280e1e2dd5c75de365ca9296236
SHY <shy9729@gmail.com>
2026-01-16 15:01:29 +0900

    style: pr 하기위해 dev랑 맞춤

--- src/main/java/com/example/lineofduty/InitData.java
+++ src/main/java/com/example/lineofduty/InitData.java
@@ -4,6 +4,7 @@ import com.example.lineofduty.common.exception.CustomException;
 import com.example.lineofduty.common.exception.ErrorMessage;
 import com.example.lineofduty.common.model.enums.ApplicationStatus;
 import com.example.lineofduty.common.model.enums.Role;
+import com.example.lineofduty.common.util.PasswordEncoder;
 import com.example.lineofduty.domain.enlistmentSchedule.repository.EnlistmentScheduleRepository;
 import com.example.lineofduty.domain.product.repository.ProductRepository;
 import com.example.lineofduty.domain.user.repository.UserRepository;
@@ -12,7 +13,6 @@ import com.example.lineofduty.entity.Product;
 import com.example.lineofduty.entity.User;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -85,6 +85,11 @@ public class InitData {
                 enlistmentScheduleRepository.save(schedule);
             }
         }
+
+        // 판매자 = 관리자
+        User productAdmin = userRepository.findByEmail(("admin@example.com")).orElseThrow(
+                ()-> new CustomException(ErrorMessage.USER_NOT_FOUND)
+        );
 
         productRepository.save(
                 new Product(
