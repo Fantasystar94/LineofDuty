5ed71a52bba7ab63b91da35c0747c5faab6debaa
fantasystar94 <aniseed_3@naver.com>
2026-01-20 17:19:07 +0900

    feat: 입영 검색 기능 추가 - 검색 기능 추가 - build.gradle 변경

--- src/main/java/com/example/lineofduty/domain/enlistmentSchedule/model/response/EnlistmentScheduleCreateResponse.java
+++ src/main/java/com/example/lineofduty/domain/enlistmentSchedule/model/response/EnlistmentScheduleCreateResponse.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor
@@ -14,13 +15,17 @@ public class EnlistmentScheduleCreateResponse {
     private Long scheduleId;
     private LocalDate enlistmentDate;
     private ApplicationStatus status;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 
     public static EnlistmentScheduleCreateResponse from(EnlistmentApplication enlistmentApplication) {
         return new EnlistmentScheduleCreateResponse(
                 enlistmentApplication.getId(),
                 enlistmentApplication.getScheduleId(),
                 enlistmentApplication.getEnlistmentDate(),
-                enlistmentApplication.getApplicationStatus()
+                enlistmentApplication.getApplicationStatus(),
+                enlistmentApplication.getCreatedAt(),
+                enlistmentApplication.getModifiedAt()
         );
     }
 }
