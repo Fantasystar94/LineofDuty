7bca7cfc39a2f058711e2b37003c06c165333471
SHY <shy9729@gmail.com>
2026-01-28 21:29:29 +0900

    refactor: password 패턴에 조건 추가
    NotBlank
    Size 추가

--- src/main/java/com/example/lineofduty/domain/user/dto/UserUpdateRequest.java
+++ src/main/java/com/example/lineofduty/domain/user/dto/UserUpdateRequest.java
@@ -2,6 +2,7 @@ package com.example.lineofduty.domain.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -14,6 +15,8 @@ public class UserUpdateRequest {
     @NotBlank(message = "본인 확인을 위해 기존 비밀번호를 입력해주세요.")
     private String currentPassword;
 
+    @NotBlank
+    @Size(min = 8, max = 20)
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@!%*#?&])[A-Za-z\\d$@!%*#?&]{8,20}$",
             message = "비밀번호는 8~20자 영문, 숫자, 특수문자를 포함해야 합니다.")
     private String newPassword;
