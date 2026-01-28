56dc89ac34b332f3a1f27265b7c7ef466f83ec99
SHY <shy9729@gmail.com>
2026-01-28 21:24:53 +0900

    style: 중복으로 입력된 내용 수정
    ''

--- src/main/java/com/example/lineofduty/domain/user/dto/UserUpdateRequest.java
+++ src/main/java/com/example/lineofduty/domain/user/dto/UserUpdateRequest.java
@@ -14,7 +14,7 @@ public class UserUpdateRequest {
     @NotBlank(message = "본인 확인을 위해 기존 비밀번호를 입력해주세요.")
     private String currentPassword;
 
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,20}$",
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@!%*#?&])[A-Za-z\\d$@!%*#?&]{8,20}$",
             message = "비밀번호는 8~20자 영문, 숫자, 특수문자를 포함해야 합니다.")
     private String newPassword;
 }
